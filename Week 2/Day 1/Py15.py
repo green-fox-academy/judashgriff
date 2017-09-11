340a01763e64a6caf6a4e121d0ae2d44b6f83c28
András Krausz <Andras.Krausz@gmail.com>
2017-09-11 15:06:25 +0200

    exercise 15 is done.

---
+++ Week 2/Day 1/Py15.py
@@ -0,0 +1,7 @@
+# Write a program that asks for an integer that is a distance in kilometers,
+# then it converts that value to miles and prints it
+
+print ("What distance will you travel today? How many kilometers?")
+km = int(input())
+mile = km*0.62
+print ("It will be about " + str(mile) + " miles.")
