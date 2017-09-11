63d14bdfc259e0b3c5bd558f4d4ec6ff66536f52
András Krausz <Andras.Krausz@gmail.com>
2017-09-11 12:09:31 +0200

    exercise 3 is done.

--- Week 2/Day 1/py3.py
+++ Week 2/Day 1/py3.py
@@ -1,6 +1,6 @@
 # Greet 3 of your classmates with this program, in three separate lines
 # like:
 #
-Print ("Hello, Ákos!")
-Print ("Hello, Anna!")
-Print ("Hello, Barna!")
+print ("Hello, Ákos!")
+print ("Hello, Anna!")
+print ("Hello, Barna!")
