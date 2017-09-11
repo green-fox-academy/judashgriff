c1b4e0efa774faa5aa2437fa56372cd762daa1b5
András Krausz <Andras.Krausz@gmail.com>
2017-09-11 12:07:47 +0200

    exercise 1-2 is done, started nr. 3

---
+++ Week 2/Day 1/Practice.py
@@ -0,0 +1,13 @@
+Student_count = 100
+Student_per_class =20
+
+#while Student_count > 0:
+#    print('hello world', Student_count)
+#    Student_count -= 1
+
+print(range(3) )
+0
+1
+2
+for i in range(3):
+    print(i) 
---
+++ Week 2/Day 1/py3.py
@@ -0,0 +1,6 @@
+# Greet 3 of your classmates with this program, in three separate lines
+# like:
+#
+Print ("Hello, Ákos!")
+Print ("Hello, Anna!")
+Print ("Hello, Barna!")
