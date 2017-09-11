f8f593dcf93a29658b91726a872fd454d400d6e4
András Krausz <Andras.Krausz@gmail.com>
2017-09-11 15:33:56 +0200

    exercise 18 is done.

---
+++ Week 2/Day 1/Py18.py
@@ -0,0 +1,10 @@
+# Write a program that reads a number form the standard input,
+# Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+print ("Please, provide a number of your choice.")
+num = int(input())
+
+if num%2 == 0:
+    print ("This is an even number.")
+else:
+    print ("this is an odd number")
