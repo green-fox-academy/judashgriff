057811b5adf8c994cbf35fecc24d3665a66cc323
András Krausz <Andras.Krausz@gmail.com>
2017-09-11 13:38:00 +0200

    exercise 10 is done.

---
+++ Week 2/Day 1/Py10.py
@@ -0,0 +1,18 @@
+# Define several things as a variable than print their values
+# Your name as a string
+# Your age as an integer
+# Your height in meters as a double
+# Wether you are married or not as a boolean
+
+name = 'Krausz Andras'
+age = 27
+height = 1.9
+married = False
+
+print (name + " is my name")
+print ("I am " + str(age) + " years old.")
+print ("I am " +  str(height) + " meters tall")
+print ("Am I married? " + str(married) )
+
+
+print (name + " years old. " + str(height) + " meters tall; " + str(married) + " - if he is married.")
