4f616c516a3513045e07a5f4376885d0486a004a
András Krausz <Andras.Krausz@gmail.com>
2017-09-12 11:07:20 +0200

    Done with doubling and greeting exercise

---
+++ Week 2/Day 2/Py2.py
@@ -0,0 +1,11 @@
+# - Create variable named `al` and assign the value `Greenfox` to it
+# - Create a function called `greet` that greets it's input parameter
+#     - Greeting is printing e.g. `Greetings, dear Greenfox`
+# - Greet `al`
+
+al = "Greenfox"
+
+def greet (user):
+    return "Greetings, dear " + user +  "!"
+
+print (greet (al))
