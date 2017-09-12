588318e7905d3fbfc6d62bb73e870362a26a42f2
András Krausz <Andras.Krausz@gmail.com>
2017-09-12 11:38:32 +0200

    Done with printer exercise

---
+++ Week 2/Day 2/Py6.py
@@ -0,0 +1,8 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer (*arg):
+    return arg
+
+print (printer('kutya, ' 'cica, ' 'béka'))
