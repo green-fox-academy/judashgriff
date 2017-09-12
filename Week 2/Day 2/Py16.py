6957b261b7ab2b0668a37c35293def65f2e57a0f
András Krausz <Andras.Krausz@gmail.com>
2017-09-12 16:14:18 +0200

    Done with appending animal names with +a.

---
+++ Week 2/Day 2/Py16.py
@@ -0,0 +1,9 @@
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macs", "cic"]`
+# - Add all elements an `"a"` at the end
+
+nimals = ["kuty", "macs", "cic"]
+
+new_list = ['%sa' % x for x in nimals]
+
+print (new_list)
