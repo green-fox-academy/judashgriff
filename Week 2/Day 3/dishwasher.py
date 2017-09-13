5ff1e476251f50d0195039ca529381fe967f8d02
András Krausz <Andras.Krausz@gmail.com>
2017-09-13 10:40:40 +0200

    Feat (dishwasher.py) created file with 3 solutions.

---
+++ Week 2/Day 3/dishwasher.py
@@ -0,0 +1,13 @@
+example = "In a dishwasher far far away"
+
+# I would like to replace "dishwasher" with "galaxy" in this example
+# Please fix it for me!
+# Expected ouput: In a galaxy far far away
+
+example = example.replace("dishwasher", "galaxy")
+
+# example = (example[:5] + "galaxy " + example[16:])
+
+print (example)
+
+# print(example[:5]+"galaxy "+example[16:])
