fd0c6d5f6440b50f8cdb366a11094f52d50ebef9
András Krausz <Andras.Krausz@gmail.com>
2017-09-13 15:23:10 +0200

    Feat (dictionary_second.py) created file with solution.

--- Week 2/Day 3/dictionary_second.py
+++ Week 2/Day 3/dictionary_second.py
@@ -1,4 +1,3 @@
-
 students = [
         {'name': 'Rezso', 'age': 9.5, 'candies': 2},
         {'name': 'Gerzson', 'age': 10, 'candies': 1},
@@ -11,3 +10,30 @@ students = [
 
 # create a function that takes a list of students and prints: 
 #  - how many candies they have on average
+
+def morecandy(students):
+    candy = []
+    for i in range(len(students)):
+        if students[i]['candies'] > 4:
+            candy.append(students[i]['name'])
+            candy.append(students[i]['candies'])
+    return candy
+
+def average(students):
+    total = []
+    for i in range(len(students)):
+        total.append(students[i]['candies'])
+    return total
+        
+more_than4 = morecandy(students)
+total = average(students)
+
+sum_of_total = 0
+
+for i in range(len(total)):
+    sum_of_total += total[i]
+
+average_candy = sum_of_total / len(total)
+
+print (more_than4)
+print (average_candy)
