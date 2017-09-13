6005b6da5029fd5f3d31ed70b98845e34a93f59f
András Krausz <Andras.Krausz@gmail.com>
2017-09-13 15:45:51 +0200

    Feat(todo.py): created new file and provided solution.

---
+++ Week 2/Day 3/todo.py
@@ -0,0 +1,16 @@
+# Add "My todo:" to the beginning of the todoText
+# Add " - Download games" to the end of the todoText
+# Add " - Diablo" to the end of the todoText but with indention
+
+# Expected outpt:
+
+# My todo:
+#  - Buy milk
+#  - Download games
+#      - Diablo
+
+indent = "    "
+
+todoText = "My todo:\n  - Buy milk\n  - Download games\n" + indent + " - Diablo"
+
+print(todoText)
