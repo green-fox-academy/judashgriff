7592994e338d4c653550c77e7c30e5920517ecf3
András Krausz <Andras.Krausz@gmail.com>
2017-09-14 16:28:24 +0200

    Feat(table_printer.py): created new file for exercise and began the programming.

---
+++ Week 2/Day 3/table_printer.py
@@ -0,0 +1,92 @@
+# Create a function that prints the ingredient list of dictionaries to the console in the following format:
+#
+# +--------------------+---------------+----------+
+# | Ingredient         | Needs cooling | In stock |
+# +--------------------+---------------+----------+
+# | vodka              | Yes           | 1        |
+# | coffee_liqueur     | Yes           | -        |
+# | fresh_cream        | Yes           | 1        |
+# | captain_morgan_rum | Yes           | 2        |
+# | mint_leaves        | No            | -        |
+# +--------------------+---------------+----------+
+#
+# OPTIONAL:
+# The frist columns should be automatically as wide as the longest key
+
+ingredients = [
+	{ "name": "vodka", "in_stock": 1, "needs_cooling": True },
+	{ "name": "coffee_liqueur", "in_stock": 0, "needs_cooling": True },
+	{ "name": "fresh_cream", "in_stock": 1, "needs_cooling": True },
+	{ "name": "captain_morgan_rum", "in_stock": 2, "needs_cooling": True },
+	{ "name": "mint_leaves", "in_stock": 0, "needs_cooling": False },
+	{ "name": "sugar", "in_stock": 0, "needs_cooling": False },
+	{ "name": "lime juice", "in_stock": 0, "needs_cooling": True },
+	{ "name": "soda", "in_stock": 0, "needs_cooling": True }
+]
+
+def width_collector(item):
+    List = []
+    for i in item:
+        List.append(i['name'])
+    width_counter(List)
+    return
+
+
+def width_counter(List_item):
+    first_column = len(max(List_item, key=len))
+    printer(first_column)
+    return
+
+
+def printer(first):
+    linear = "+" 
+    for i in range(first):
+        linear += "-"
+    linear += "+"
+    second_and_third_column(linear)
+    return
+    
+    
+def second_and_third_column(linear):
+    second_column = 15
+    third_column = 10
+    for i in range(second_column):
+        linear += "-"
+    linear += "+"
+    for i in range(third_column):
+        linear += "-"
+    linear += "+"
+    print(linear)
+    header(linear, second_column, third_column)
+    return linear
+
+
+def header(first, second, third):
+    header = "|"
+    ingridient = " Ingredient "
+    cooling = " Needs cooling "
+    stock = " In stock "
+    header += ingridient
+    more = (len(first) - len(ingridient)) * "-"
+    header += more 
+    header += "|" + cooling + "|" + stock + "|"
+    print(header)
+    print(first)
+    # content(first, second, third)
+    return
+
+
+# def content(first, second, third):
+
+
+
+linear = width_collector(ingredients)
+
+# printer(ingredients)
+
+
+
+
+
+
+
