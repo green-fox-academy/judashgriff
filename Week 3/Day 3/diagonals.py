8a2e5b82374768234a4a81a16130ea8b896fb383
András Krausz <Andras.Krausz@gmail.com>
2017-09-20 11:35:02 +0200

    Feat(diagonals.py): Created new file for exercise with solution.

---
+++ Week 3/Day 3/diagonals.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# draw the canvas' diagonals in green.
+
+green_diagonal = canvas.create_line(0, 0, 300, 300, fill='green')
+green_diagonal = canvas.create_line(0, 300, 300, 0, fill='green')
+
+
+
+root.mainloop()
