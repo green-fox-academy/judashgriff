1b00592dfc0194d6666e57d7bd40d519d913ee99
András Krausz <Andras.Krausz@gmail.com>
2017-09-20 16:55:22 +0200

    Feat(line_play_quarters.py): Created new file with solution.

--- Week 3/Day 3/line_play_quarters.py
+++ Week 3/Day 3/line_play_quarters.py
@@ -9,4 +9,27 @@ canvas.pack()
 # and repeat this pattern in each quarter:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
+def make_colorful_lines():
+    for each in range(15):
+        canvas.create_line(0, 10 + (each - 1) * 10, 10 + (each - 1) * 10, 150, fill='green')
+    for each in range(15):
+        canvas.create_line(150 + (each - 1) * 10, 0, 300, 10 + (each - 1) * 10, fill='purple')
+    for each in range(15):
+        canvas.create_line(0, 160 + (each - 1) * 10, 10 + (each - 1) * 10, 300, fill='green')
+    for each in range(15):
+        canvas.create_line(150 + (each - 1) * 10, 150, 300, 160 + (each - 1) * 10, fill='purple')
+    for each in range(15):
+        canvas.create_line(10 + (each - 1) * 10, 0, 150, 10 + (each - 1) * 10, fill='purple')
+    for each in range(15):
+        canvas.create_line(150, 10 + (each - 1) * 10, 160 + (each - 1) * 10, 150, fill='green')
+    for each in range(15):
+        canvas.create_line(10 + (each - 1) * 10, 150, 150, 160 + (each - 1) * 10, fill='purple')
+    for each in range(15):
+        canvas.create_line(150, 160 + (each - 1) * 10, 160 + (each - 1) * 10, 300, fill='green')
+
+
+make_colorful_lines()
+
+
+
 root.mainloop()
