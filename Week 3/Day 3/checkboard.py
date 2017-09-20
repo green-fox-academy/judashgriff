61d5d5b2a76ce5a6e580ce6f5095a90cc0322ad4
András Krausz <Andras.Krausz@gmail.com>
2017-09-20 15:12:21 +0200

    Feat(checkboard.py): Created new file for exercise with solution.

---
+++ Week 3/Day 3/checkboard.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# fill the canvas with a checkerboard pattern.
+
+
+def create_checkboard(checkboard):
+    for each in range(9):
+        box = canvas.create_rectangle(0 + (each - 1) * checkboard, 0 + (each - 1) * checkboard, checkboard * each, checkboard * each, fill='black')
+    return create_down(checkboard), create_right(checkboard)
+
+
+def create_down(checkboard):
+    for each in range(7):
+        box = canvas.create_rectangle(0 + (each - 1) * checkboard, 0 + checkboard * 2 + (each - 1) * checkboard, checkboard * each, checkboard * 2 + checkboard * each, fill='black')
+    for each in range(5):
+        box = canvas.create_rectangle(0 + (each - 1) * checkboard, 0 + checkboard * 4 + (each - 1) * checkboard, checkboard * each, checkboard * 4 + checkboard * each, fill='black')
+    for each in range(3):
+        box = canvas.create_rectangle(0 + (each - 1) * checkboard, 0 + checkboard * 6 + (each - 1) * checkboard, checkboard * each, checkboard * 6 + checkboard * each, fill='black')
+    return
+
+
+def create_right(checkboard):
+    for each in range(7):
+        box = canvas.create_rectangle(0 + checkboard * 2 + (each - 1) * checkboard, 0 + (each - 1) * checkboard, checkboard * 2 + checkboard * each, checkboard * each, fill='black')
+    for each in range(5):
+        box = canvas.create_rectangle(0 + checkboard * 4 + (each - 1) * checkboard, 0 + (each - 1) * checkboard, checkboard * 4 + checkboard * each, checkboard * each, fill='black')
+    for each in range(3):
+        box = canvas.create_rectangle(0 + checkboard * 6 + (each - 1) * checkboard, 0 + (each - 1) * checkboard, checkboard * 6 + checkboard * each, checkboard * each, fill='black')
+    return
+
+checkboard = 300/8
+
+create_checkboard(checkboard)
+
+root.mainloop()
