c89c9f8e2428c18e7c537d89692e8361970c0556
András Krausz <Andras.Krausz@gmail.com>
2017-09-21 11:03:13 +0200

    Feat(sumdigit.py): Created new file with solution.

--- Week 3/Day 4/numberadder.py
+++ Week 3/Day 4/numberadder.py
@@ -1,15 +1,12 @@
 # Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
 
-def number_adder(n, out):
-    base = 1
-    if base == n:
-        out += 1
-        print(out)
-        return
+def number_adder(n):
+    if 1 == n:
+        return n
     else:
-        out += n
-        number_adder(n-1, out)
-out = 0
+        return n + number_adder(n-1)
 
-n = 980
-number_adder(n, out)
+n = 3
+sum_of_total = number_adder(n)
+
+print(sum_of_total)
---
+++ Week 3/Day 4/sumdigit.py
@@ -0,0 +1,15 @@
+# Given a non-negative int n, return the sum of its digits recursively (no loops). 
+# Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+# divide (/) by 10 removes the rightmost digit (126 // 10 is 12).
+
+def get_sumdigit(n):
+    if n < 10:
+        return n
+    else:
+        return n % 10 + get_sumdigit(n//10)
+
+
+n = 1886561653
+sumdigit = get_sumdigit(n)
+
+print(sumdigit)
