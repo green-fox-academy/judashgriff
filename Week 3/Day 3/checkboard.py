cee5413e29af0adef4aaf65a76e4dc41e0c552c2
András Krausz <Andras.Krausz@gmail.com>
2017-09-22 11:06:36 +0200

    Refactor(checkboard.py): created elegant and more efficient solution.

--- Week 3/Day 3/checkboard.py
+++ Week 3/Day 3/checkboard.py
@@ -2,47 +2,19 @@ from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300')
+canvas = Canvas(root, width='300', height='300', highlightthickness=0)
 canvas.pack()
 
-# fill the canvas with a checkerboard pattern.
+
+def create_checkboard(size):
+    for row in range(8):
+        for column in range(8):
+            if (row + column) % 2 == 0:
+                canvas.create_rectangle(column * size, row * size, (column + 1) * size, (row + 1) * size, fill='black')
 
 
-# def create_checkboard(checkboard):
-#     for each in range(9):
-#         box = canvas.create_rectangle(0 + (each - 1) * checkboard, 0 + (each - 1) * checkboard, checkboard * each, checkboard * each, fill='black')
-#     return create_down(checkboard), create_right(checkboard)
+size = 300/8
 
-
-# def create_down(checkboard):
-#     for each in range(7):
-#         box = canvas.create_rectangle(0 + (each - 1) * checkboard, 0 + checkboard * 2 + (each - 1) * checkboard, checkboard * each, checkboard * 2 + checkboard * each, fill='black')
-#     for each in range(5):
-#         box = canvas.create_rectangle(0 + (each - 1) * checkboard, 0 + checkboard * 4 + (each - 1) * checkboard, checkboard * each, checkboard * 4 + checkboard * each, fill='black')
-#     for each in range(3):
-#         box = canvas.create_rectangle(0 + (each - 1) * checkboard, 0 + checkboard * 6 + (each - 1) * checkboard, checkboard * each, checkboard * 6 + checkboard * each, fill='black')
-#     return
-
-
-# def create_right(checkboard):
-#     for each in range(7):
-#         box = canvas.create_rectangle(0 + checkboard * 2 + (each - 1) * checkboard, 0 + (each - 1) * checkboard, checkboard * 2 + checkboard * each, checkboard * each, fill='black')
-#     for each in range(5):
-#         box = canvas.create_rectangle(0 + checkboard * 4 + (each - 1) * checkboard, 0 + (each - 1) * checkboard, checkboard * 4 + checkboard * each, checkboard * each, fill='black')
-#     for each in range(3):
-#         box = canvas.create_rectangle(0 + checkboard * 6 + (each - 1) * checkboard, 0 + (each - 1) * checkboard, checkboard * 6 + checkboard * each, checkboard * each, fill='black')
-#     return
-
-
-def create_checkboard(checkboard):
-    for i in range(9):
-        for j in range(9):
-            canvas.create_rectangle(0 + (j*2) * checkboard, 0 + (i*2) * checkboard, checkboard + (j*2) * checkboard,  checkboard + (i*2) * checkboard, fill='black')
-            canvas.create_rectangle(checkboard + (j*2) * checkboard, checkboard + (i*2) * checkboard, 2*checkboard + checkboard * (j*2), 2*checkboard + checkboard * (j*2), fill='black')                
-
-
-checkboard = 305/8
-
-create_checkboard(checkboard)
+create_checkboard(size)
 
 root.mainloop()
