d30341039545b5a40a7c0c824021805d5f40f25d
András Krausz <Andras.Krausz@gmail.com>
2017-09-27 16:21:21 +0200

    Feat(extension.py, test.py): Solution for sixth exercise is provided.

---
+++ Week 4/Day 3/extend.py
@@ -0,0 +1,26 @@
+import statistics
+
+# Adds a and b, returns as result
+def add(a, b):
+    c = a + b
+    return c
+
+# Returns the highest value from the three given params
+def max_of_three(a, b, c):
+    return max(a, b, c)
+
+# Returns the median value of a list given as param
+def median(pool):
+    return statistics.median(pool)
+
+# Returns true if the param is a vovel
+def is_vovel(char):
+    return char.lower() in 'aeiouéáőűöüóí'
+
+# Create a method that translates hungarian into the teve language
+def translate(hungarian):
+    teve = hungarian
+    for char in teve:
+        if is_vovel(char):
+            teve = (char+'v'+char).join(teve.split(char))
+    return teve
