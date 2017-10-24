79e7991cf4ff07b4a9d5580905894cfa65f70158
András Krausz <Andras.Krausz@gmail.com>
2017-10-24 11:58:15 +0200

    bmi

---
+++ Week 7/day 2/bmi.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let massInKg = 81.2;
+let heightInM = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+
+let bmi = massInKg / Math.pow(heightInM, 2)
+
+console.log(bmi)
