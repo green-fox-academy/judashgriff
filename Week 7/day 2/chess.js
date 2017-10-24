3c454f317ade3e966a3492f5b79d78f52af0aa53
András Krausz <Andras.Krausz@gmail.com>
2017-10-24 17:15:55 +0200

    chess

---
+++ Week 7/day 2/chess.js
@@ -0,0 +1,25 @@
+// Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % % 
+//  % % % %
+//
+
+let size = 8
+
+if (size % 2 == 1){
+    size = size-1
+}
+
+for (let i = 1; i <= size; i++) {
+    if (i % 2 == 1) {
+        console.log("% ".repeat(size/2));
+    } else {
+        console.log(" %".repeat(size/2));
+    }
+}
