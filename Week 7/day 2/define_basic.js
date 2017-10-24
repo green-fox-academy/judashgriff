641ff20ec168e21448c946d2bf02a2ef00c05567
András Krausz <Andras.Krausz@gmail.com>
2017-10-24 12:01:45 +0200

    define basic info

---
+++ Week 7/day 2/define_basic.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = "Krausz András";
+let age = 27;
+let height = 1.90;
+let married = false;
+
+console.log(name + age + height + married);
