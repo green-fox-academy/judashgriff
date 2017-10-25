6464aeea31b6a21eed1d6dd443cf14772083e614
András Krausz <Andras.Krausz@gmail.com>
2017-10-25 14:32:04 +0200

    reverse array

--- Week 7/day 3/reverse_list.js
+++ Week 7/day 3/reverse_list.js
@@ -5,3 +5,15 @@
 // 		- do it with the built in method
 //		- do it with creating a new temp array and a loop
 // - Print the elements of the reversed `aj`
+
+let aj = [3, 4, 5, 6, 7];
+
+let newAj = aj.reverse()
+
+console.log(newAj)
+
+let newAj2 = []
+for (let i = aj.length - 1; i >= 0; i--) {
+    newAj2.unshift(aj[i]);
+}
+console.log(newAj2)
