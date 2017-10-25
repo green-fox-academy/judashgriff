dd3a57e286fd62cfdf579b9ea0276bde49618e53
András Krausz <Andras.Krausz@gmail.com>
2017-10-25 11:53:32 +0200

    greetings

---
+++ Week 7/day 3/greeter.js
@@ -0,0 +1,18 @@
+'use strict';
+// - Create variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings, dear Greenfox`
+//     - Prepare for the special case when no parameters are given
+// - Greet `al`
+
+let al = "Greenfox"
+
+let greet = function greet(inp) {
+    if (inp === undefined) {
+        console.log("Greetings, dear visitor!")
+    } else {
+        console.log("Greetings, dear " + inp)
+    }
+}
+
+greet(al)
