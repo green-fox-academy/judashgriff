801e007b4d5c06e01680542a096afc88f1aa475c
András Krausz <Andras.Krausz@gmail.com>
2017-10-25 13:52:49 +0200

    change_elements

---
+++ Week 7/day 3/change_element.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create an array named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
+// - Change the 8 to 4 with the `.map` method 
+// - Print the fourth element as a test
+
+let s = [1, 2, 3, 8, 5, 6];
+
+s.map(function(x) {
+    if (x === 4) {
+        s[x] = 8;
+    }
+})
+
+console.log(s[3])
