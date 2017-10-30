baeb3d09d1ad45af4d4c8c935529867155305ca7
András Krausz <Andras.Krausz@gmail.com>
2017-10-30 11:42:43 +0100

    Petrol station exercise

---
+++ Week 8/petrol.js
@@ -0,0 +1,50 @@
+'use strict';
+// Create and object called car
+//  - It should store its petrol level called petrolLevel
+//  - It should store its petrol capacity called petrolCapacity
+//  - It should have a refill(amount) method, that increments the petrol level,
+//    than returns how much petrol it consumed from the given amount
+//  - Initialize the petrol level to zero and the capacity to 50 
+//
+// Create an object called station
+//  - It should store petrol amount called petrolStorage
+//  - It should have a provide(car) method that calls the refill method of the car
+//    with the stored petrol amount as a parameter, then decrement the used petrol
+//  - Initialize the petrol amount to 3000
+
+let car = {
+    petrolLevel: 0,
+    petrolCapacity: 50,
+    refill: function(amt) {
+        let toFuel = this.petrolCapacity - this.petrolLevel;
+        if (amt >= toFuel) {
+            this.petrolLevel += toFuel;
+            amt -= toFuel;
+            return amt
+        } else {
+            this.petrolLevel += amt;
+            amt = 0;
+            return amt
+        }
+    }
+}
+
+let station = {
+    petrolStorage: 3000,
+    provide: function(car) {
+        let newPetrol = car.refill(this.petrolStorage);
+        this.petrolStorage = newPetrol;
+    }
+}
+
+
+
+
+
+console.log(car.petrolLevel);
+console.log(station.petrolStorage);
+
+station.provide(car);
+
+console.log(car.petrolLevel);
+console.log(station.petrolStorage);
