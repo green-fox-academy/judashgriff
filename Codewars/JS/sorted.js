710e683fe75266b4c1cd21dd7097494f336ac964
András Krausz <Andras.Krausz@gmail.com>
2018-03-21 09:58:44 +0100

    sorting is done with two solutions

--- Codewars/JS/sorted.js
+++ Codewars/JS/sorted.js
@@ -1,39 +1,44 @@
-function sortAscending(arr) {
-  return arr.sort(function(a, b) {
-    return a - b;
-  });
-}
+// function sortAscending(arr) {
+//   return arr.sort(function(a, b) {
+//     return a - b;
+//   });
+// }
 
-function sortDescending(arr) {
-  return arr.sort(function(a, b) {
-    return b - a;
-  });
-}
+// function sortDescending(arr) {
+//   return arr.sort(function(a, b) {
+//     return b - a;
+//   });
+// }
 
-function isSortedAndHow(array) {
-  let how = "no";
-  if (array.length < 2) {
-    return how;
-  }
-  let asc = sortAscending([...array]);
-  let desc = sortDescending(array.slice());
-  console.log(array, asc, desc);
-  if (asc == array) {
-    how = "yes, ascending";
-  }
-  if (desc == array) {
-    how = "yes, descending";
-  }
+// function isSortedAndHow(array) {
+//   let how = "no";
+//   if (array.length < 2) { return how; }
+//   let asc = sortAscending([...array]);  // using the spread operator in an array
+//   let desc = sortDescending(array.slice());  // using slice - does the same it this case
+//   let checkerAsc = [];
+//   let checkerDesc = [];
+//   array.forEach((e, i) => {
+//     if (e === asc[i]) {
+//       checkerAsc.push(i);
+//     }
+//     if (e === desc[i]) {
+//       checkerDesc.push(i);
+//     }
+//   });
+//   if (array.length === checkerAsc.length) {
+//     how = "yes, ascending";
+//   }
+//   if (array.length === checkerDesc.length) {
+//     how = "yes, descending";
+//   }
   
-  return how;
+//   return how;
+// }
+
+function isSortedAndHow(arr) {
+return arr.every((e, i) => i === 0 || arr[i] <= arr[i - 1])? 'yes, descending' :
+       arr.every((e, i) => i === 0 || arr[i] >= arr[i - 1])? 'yes, ascending' : 'no';
 }
 
-let myArr = [1, 515, 1000, 1200];
+let myArr = [100, 80, 76, -13];
 console.log(isSortedAndHow(myArr));
-
-
-// const animals = ['Dog', 'Cat', 'Elephant', 'Dolphin'];
-
-// let copy = animals.slice();
-// console.log(animals, copy);
-// console.log(animals === copy, animals == copy);
